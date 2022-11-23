da8fe9cc58c9726927526302546ab2f03cbf2715
caxco93 <caxco93@gmail.com>
2022-11-23 00:24:16 -0500

    Add reversePolishNotation implementation

--- src/lib/reversePolishNotation.ts
+++ src/lib/reversePolishNotation.ts
@@ -7,7 +7,31 @@ export const isOperator = (element: RPNElement): element is Operator => {
   return (element as Operator).length !== undefined;
 };
 
+const operations = {
+  "+": (a: number, b: number) => a + b,
+  "-": (a: number, b: number) => a - b,
+  "*": (a: number, b: number) => a * b,
+  "/": (a: number, b: number) => a / b,
+  "^": (a: number, b: number) => a ** b,
+};
+
 const reversePolishNotation = (input: RPNExpression): RPNSteps => {
-  return [[]];
+  const result: RPNSteps = [];
+  const stack: number[] = [];
+  input.forEach((element) => {
+    if (isOperator(element)) {
+      const a = stack.pop();
+      const b = stack.pop();
+      if (a === undefined || b === undefined) {
+        throw Error("Insufficient operands in expression");
+      }
+      const c = operations[element](a, b);
+      stack.push(c);
+    } else {
+      stack.push(element);
+    }
+    result.push([...stack]);
+  });
+  return result;
 };
 export default reversePolishNotation;
