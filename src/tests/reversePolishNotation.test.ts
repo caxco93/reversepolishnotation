b89dd57ee26d389a4e20122f259e3a6e0f338776
caxco93 <caxco93@gmail.com>
2022-11-23 01:22:02 -0500

    Add error test case

--- src/tests/reversePolishNotation.test.ts
+++ src/tests/reversePolishNotation.test.ts
@@ -1,13 +1,21 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import reversePolishNotation, {
   RPNExpression,
 } from "@/lib/reversePolishNotation";
 
 // Edit an assertion and save to see HMR in action
 
-test("Reverse Polish Notation", () => {
-  const input: RPNExpression = [2, 2, "+"];
-  const output = reversePolishNotation(input);
-  const expected = [[2], [2, 2], [4]];
-  expect(output).toEqual(expected);
+describe("Reverse Polish Notation", () => {
+  it("handles simple expressions", () => {
+    const input: RPNExpression = [2, 2, "+"];
+    const output = reversePolishNotation(input);
+    const expected = [[2], [2, 2], [4]];
+    expect(output).toEqual(expected);
+  });
+  it("throws when short on operands", () => {
+    const input: RPNExpression = [2, 2, "+", "*"];
+    expect(() => {
+      reversePolishNotation(input);
+    }).toThrowError("Insufficient operands in expression");
+  });
 });
