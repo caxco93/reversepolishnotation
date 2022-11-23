18b6bc8309c11607c53b06f595122ae55af130b5
caxco93 <caxco93@gmail.com>
2022-11-23 01:17:34 -0500

    Reduce operations arguments verbose types

--- src/lib/reversePolishNotation.ts
+++ src/lib/reversePolishNotation.ts
@@ -1,4 +1,5 @@
 type Operator = "+" | "-" | "*" | "/" | "^";
+type Operation = (a: number, b: number) => number;
 export type RPNElement = number | Operator;
 export type RPNExpression = Array<RPNElement>;
 type RPNSteps = Array<Array<number>>;
@@ -7,12 +8,12 @@ export const isOperator = (element: RPNElement): element is Operator => {
   return (element as Operator).length !== undefined;
 };
 
-const operations = {
-  "+": (a: number, b: number) => a + b,
-  "-": (a: number, b: number) => a - b,
-  "*": (a: number, b: number) => a * b,
-  "/": (a: number, b: number) => a / b,
-  "^": (a: number, b: number) => a ** b,
+const operations: Record<Operator, Operation> = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+  "^": (a, b) => a ** b,
 };
 
 const reversePolishNotation = (input: RPNExpression): RPNSteps => {
@@ -23,7 +24,7 @@ const reversePolishNotation = (input: RPNExpression): RPNSteps => {
       const a = stack.pop();
       const b = stack.pop();
       if (a === undefined || b === undefined) {
-        throw Error("Insufficient operands in expression");
+        throw new Error("Insufficient operands in expression");
       }
       const c = operations[element](a, b);
       stack.push(c);
