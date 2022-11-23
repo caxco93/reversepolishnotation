23702812b0b3b8253c87f89d187898c0b2325721
Orlando Benjamin <caxco93@gmail.com>
2022-11-23 01:47:24 -0500

    Implement Reverse Polish Notation logic (#2)
    * Create base isOperator function
    
    * Add isOperator tests
    
    * Implement isOperator type guard
    
    * Add reversePolishNotation implementation
    
    * Reduce operations arguments verbose types
    
    * Add error test case
    
    * Add complex case test

--- src/lib/reversePolishNotation.ts
+++ src/lib/reversePolishNotation.ts
@@ -1,9 +1,38 @@
 type Operator = "+" | "-" | "*" | "/" | "^";
-type RPNElement = number | Operator;
+type Operation = (a: number, b: number) => number;
+export type RPNElement = number | Operator;
 export type RPNExpression = Array<RPNElement>;
 type RPNSteps = Array<Array<number>>;
 
+export const isOperator = (element: RPNElement): element is Operator => {
+  return (element as Operator).length !== undefined;
+};
+
+const operations: Record<Operator, Operation> = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+  "^": (a, b) => a ** b,
+};
+
 const reversePolishNotation = (input: RPNExpression): RPNSteps => {
-  return [[]];
+  const result: RPNSteps = [];
+  const stack: number[] = [];
+  input.forEach((element) => {
+    if (isOperator(element)) {
+      const b = stack.pop();
+      const a = stack.pop();
+      if (a === undefined || b === undefined) {
+        throw new Error("Insufficient operands in expression");
+      }
+      const c = operations[element](a, b);
+      stack.push(c);
+    } else {
+      stack.push(element);
+    }
+    result.push([...stack]);
+  });
+  return result;
 };
 export default reversePolishNotation;
--- src/tests/reversePolishNotation.test.ts
+++ src/tests/reversePolishNotation.test.ts
@@ -1,13 +1,39 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import reversePolishNotation, {
   RPNExpression,
 } from "@/lib/reversePolishNotation";
 
 // Edit an assertion and save to see HMR in action
 
-test("Reverse Polish Notation", () => {
-  const input: RPNExpression = [2, 2, "+"];
-  const output = reversePolishNotation(input);
-  const expected = [[2], [2, 2], [4]];
-  expect(output).toEqual(expected);
+describe("Reverse Polish Notation", () => {
+  it("handles simple expressions", () => {
+    const input: RPNExpression = [2, 2, "+"];
+    const output = reversePolishNotation(input);
+    const expected = [[2], [2, 2], [4]];
+    expect(output).toEqual(expected);
+  });
+  it("handles complex expressions", () => {
+    const input: RPNExpression = [2, 5, "*", 6, "+", 3, 2, "^", 1, "-", "/"];
+    const output = reversePolishNotation(input);
+    const expected = [
+      [2],
+      [2, 5],
+      [10],
+      [10, 6],
+      [16],
+      [16, 3],
+      [16, 3, 2],
+      [16, 9],
+      [16, 9, 1],
+      [16, 8],
+      [2],
+    ];
+    expect(output).toEqual(expected);
+  });
+  it("throws when short on operands", () => {
+    const input: RPNExpression = [2, 2, "+", "*"];
+    expect(() => {
+      reversePolishNotation(input);
+    }).toThrowError("Insufficient operands in expression");
+  });
 });
