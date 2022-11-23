91590030d37b8c826afbf5d0318e8c9cc9f38d13
caxco93 <caxco93@gmail.com>
2022-11-23 00:07:21 -0500

    Create base isOperator function

--- src/lib/reversePolishNotation.ts
+++ src/lib/reversePolishNotation.ts
@@ -3,6 +3,8 @@ type RPNElement = number | Operator;
 export type RPNExpression = Array<RPNElement>;
 type RPNSteps = Array<Array<number>>;
 
+export const isOperator = (element: RPNElement) => {};
+
 const reversePolishNotation = (input: RPNExpression): RPNSteps => {
   return [[]];
 };
