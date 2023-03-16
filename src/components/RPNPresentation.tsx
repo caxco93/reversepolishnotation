db319243299c0b392ceccfae71234f883fdcacce
caxco93 <caxco93@gmail.com>
2023-03-16 14:29:18 -0500

    Add stack visualization using longest step

--- src/components/RPNPresentation.tsx
+++ src/components/RPNPresentation.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import reversePolishNotation, {
+  RPNElement,
   RPNExpression,
   RPNSteps,
 } from "@/lib/reversePolishNotation";
@@ -9,15 +10,36 @@ type Props = {
   rpnExpression: RPNExpression;
 };
 
+function findLongestStep(steps: RPNSteps): Array<RPNElement> {
+  return steps.reduce((previous, current) => {
+    if (current.length > previous.length) {
+      return current;
+    } else {
+      return previous;
+    }
+  }, steps[0]);
+}
+
 function RPNPresentation({ rpnExpression }: Props) {
-  let rpnSteps: RPNSteps = [];
-  rpnSteps = reversePolishNotation(rpnExpression);
+  const rpnSteps: RPNSteps = reversePolishNotation(rpnExpression);
+  const longest = rpnSteps.length === 0 ? [] : findLongestStep(rpnSteps);
+
+  const containerStyle: CSSProperties = {
+    height: `${longest.length * 64}px`,
+  };
+
   return (
     <div className="RPNPresentation">
-      {rpnSteps.map((step, i) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={i}>{step.join(" ")}</div>
-      ))}
+      <div className="container" style={containerStyle}>
+        <div className="boxes">
+          {longest.map((element, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index} className="box">
+              {element}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
