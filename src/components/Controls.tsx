086eef852b403f293f70a048f4368167fb6f7ef1
caxco93 <caxco93@gmail.com>
2023-03-17 13:15:58 -0500

    Refactor previous and next step callback

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -8,26 +8,39 @@ type Props = {
 
 function Controls({ step, setStep, stepsLength }: Props) {
   const maxStep = Math.max(0, stepsLength - 1);
+
+  const previousStep = () => {
+    setStep((x) => Math.max(0, x - 1));
+  };
+  const nextStep = () => {
+    setStep((x) => Math.min(x + 1, maxStep));
+  };
   return (
     <div className="buttons">
       <button
         type="button"
         onClick={() => {
-          setStep((x) => Math.max(0, x - 1));
+          setStep(0);
         }}
       >
-        ←
+        ⏪
+      </button>
+      <button type="button" onClick={previousStep}>
+        ⬅️
       </button>
       <span>
         {step + 1}/{maxStep + 1}
       </span>
+      <button type="button" onClick={nextStep}>
+        ➡️
+      </button>
       <button
         type="button"
         onClick={() => {
-          setStep((x) => Math.min(x + 1, maxStep));
+          setStep(maxStep);
         }}
       >
-        →
+        ⏩
       </button>
     </div>
   );
