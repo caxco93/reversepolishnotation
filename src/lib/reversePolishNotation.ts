3118302a3cf97dc5493752fa58a0cbf4b4f8184f
caxco93 <caxco93@gmail.com>
2023-04-23 16:40:50 -0500

    Add error handling to solution and parse functions

--- src/lib/reversePolishNotation.ts
+++ src/lib/reversePolishNotation.ts
@@ -3,9 +3,10 @@ type Operation = (a: number, b: number) => number;
 export type RPNElement = number | Operator;
 export type RPNExpression = Array<RPNElement>;
 export type RPNSteps = Array<Array<RPNElement>>;
+export type Errorable<T> = { result: T; errors: Array<Error> };
 
 export const isOperator = (element: RPNElement): element is Operator => {
-  return (element as Operator).length !== undefined;
+  return (element as Operator).length === 1;
 };
 
 const operations: Record<Operator, Operation> = {
@@ -16,38 +17,52 @@ const operations: Record<Operator, Operation> = {
   "^": (a, b) => a ** b,
 };
 
-const reversePolishNotation = (input: RPNExpression): RPNSteps => {
-  const result: RPNSteps = [];
+const reversePolishNotation = (input: RPNExpression): Errorable<RPNSteps> => {
+  const rpnSteps: RPNSteps = [];
   const stack: number[] = [];
-  input.forEach((element, i) => {
+  const errors: Array<Error> = [];
+  for (let i = 0; i < input.length; i++) {
+    const element = input[i];
     if (isOperator(element)) {
-      result.push([...stack, element]);
+      rpnSteps.push([...stack, element]);
       const b = stack.pop();
       const a = stack.pop();
       if (a === undefined || b === undefined) {
-        throw new Error(
-          `Insufficient operands for the "${element}" operator at index ${i}`
+        errors.push(
+          new Error(
+            `Insufficient operands for the "${element}" operator at index ${i}`
+          )
         );
+        break;
       }
       const c = operations[element](a, b);
       stack.push(c);
     } else {
       stack.push(element);
     }
-    result.push([...stack]);
-  });
-  return result;
+    rpnSteps.push([...stack]);
+  }
+  return { result: rpnSteps, errors };
 };
 
-export const parseRPNExpression = (input: string): RPNExpression => {
-  return input.split(" ").map((element) => {
+export const parseRPNExpression = (input: string): Errorable<RPNExpression> => {
+  const result: RPNExpression = [];
+  const errors: Array<Error> = [];
+  const elements = input.split(" ");
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
     const number = parseFloat(element);
     if (Number.isNaN(number)) {
-      return element as Operator;
+      if (element.length > 1) {
+        errors.push(new Error(`Invalid operator "${element}" at index ${i}`));
+        break;
+      }
+      result.push(element as Operator);
     } else {
-      return number;
+      result.push(number);
     }
-  });
+  }
+  return { result, errors };
 };
 
 export default reversePolishNotation;
